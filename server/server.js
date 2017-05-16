258bef7a2e9f8af95ac47c0860e2cf74447ac09c
Eric Biersner <eric.biersner@gmail.com>
2017-05-16 13:59:42 -0700

    Add test POST /todos

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require("./db/mongoose");
-var {Todo} = require("./models/todo");
-var {User} = require("./models/user");
-
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
-//middleware
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -16,19 +14,15 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then((doc)=>{
+  todo.save().then((doc) => {
     res.send(doc);
-  }, (e)=> {
+  }, (e) => {
     res.status(400).send(e);
   });
 });
 
-
-
-
-
-
-
-app.listen(3000, () =>{
+app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
