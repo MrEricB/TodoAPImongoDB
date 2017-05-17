e94b859251ee28649ca9307f7e6bf3df8df03875
Eric Biersner <eric.biersner@gmail.com>
2017-05-16 20:53:13 -0700

    Add test and dev enviroments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 //to use promises instead of callbacks with mongoos. choose the promise library to use
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose: mongoose
